65d630ff82a1ca8ce2ddd2227aab0d0f0e4c17a7
Ayoub Amer <ayoubamerrr290@gmail.com>
2023-02-25 18:57:30 +0100

    retrieve response from online server instead of localhost

--- frontend/script.js
+++ frontend/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from backend
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://buoya-chat-ai.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
